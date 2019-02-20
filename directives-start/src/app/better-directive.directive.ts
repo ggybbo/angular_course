b9d91a4985ec80cce2d3cae5f2e4c0ec156c325e
Seo Boseok <ggy7bbo@gmail.com>
2019-02-20 19:40:21 +0900

    HostListener: mouseenter, mouseover, mouseleave HostBinding: snippet, more easy way to bind directive as binding

--- directives-start/src/app/better-directive.directive.ts
+++ directives-start/src/app/better-directive.directive.ts
@@ -1,14 +1,27 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterDirective]'
 })
 export class BetterDirectiveDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'green';
 
   constructor(private elementRef: ElementRef , private renderer: Renderer2) { } // better solution to angular directive
 
   ngOnInit() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'navy');
+  }
+
+  @HostListener('mouseenter')
+  mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'pink';
+  }
+
+  @HostListener('mouseleave')
+  mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+    this.backgroundColor = 'transparent';
   }
 
 }
