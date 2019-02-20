0c84eb157e750e542355840fc4c57033b8f4416b
Seo Boseok <ggy7bbo@gmail.com>
2019-02-21 02:18:18 +0900

    Service: 여러 component에서 인스턴스화하여 쓸수있다 Component hierarchy에 의해 인스턴스를 공유할 수 있으며 자식 Component의 providers에 Service를 다시 주입받으면 overriding이 발생하면서 부모 Service와 다른 인스턴스가 된다 해당 기능은 상황에 맞게 변경하여 쓰는 것이 중요하다

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -10,14 +11,17 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  // @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService) {
 
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    // this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateStatus(this.id, status);
     // console.log('A server status changed, new status: ' + status);
     this.loggingService.logStatusChange(status);
   }
